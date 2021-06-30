670596ae0196c9021c20384d410b6e15797cc3ad
Kolade Adegbaye <koladea@bu.edu>
2021-06-30 20:53:03 +0000

    retrieved data using the api and successfully incorporated into mysql database

--- weather.py
+++ weather.py
@@ -1,4 +1,7 @@
-import requests
+import sqlalchemy 
+from sqlalchemy import create_engine
+import requests, datetime
+import pandas as pd
 
 
 APIkey = '9d6461f964a087d98456d732ea7c5374'
@@ -9,4 +12,12 @@ url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + 
 
 response = requests.post(url)
 r= response.json()
-print(r)
+main = r['main']
+print(main)
+
+col_names = ['name','timestamp', 'temp', 'feels_like']
+df = pd.DataFrame(columns  = col_names)
+df.loc[len(df.index)] = [datetime.datetime.now(), main['temp'], main['feels_like']]
+
+engine = create_engine('mysql://root:codio@localhost/weather')
+df.to_sql('weather_table', con=engine, if_exists='replace', index=False)
